e4f7caa46086a7ac4158d26f76f4181bd084c649
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-04-19 20:04:33 -0300

    first route

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,10 @@ import express from "express";
 const PORT = 3333;
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.send({
+    message: "Welcome in NLW 05",
+  });
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
