8e3a8be11d67d24ba13e4cdaf69da1ee83803c4d
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-04-22 11:33:29 -0300

    fix mapping on entity and table

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -2,7 +2,7 @@ import { Entity, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
 
 import { v4 as uuid } from "uuid";
 
-@Entity("settings")
+@Entity("users")
 class User {
   @PrimaryColumn()
   id: string;
