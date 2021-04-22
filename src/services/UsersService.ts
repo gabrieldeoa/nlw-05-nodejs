1cd4de7b57e5d63d4631a5a66173256b918f584d
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-04-22 17:14:54 -0300

    create method findByEmail

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -2,10 +2,6 @@ import { getCustomRepository, Repository } from "typeorm";
 import { User } from "../entities/User";
 import { UsersRepository } from "../repositories/UsersRepository";
 
-interface IUsersCreate {
-  email: string;
-}
-
 class UsersService {
   private usersRepository: Repository<User>;
 
@@ -13,7 +9,7 @@ class UsersService {
     this.usersRepository = getCustomRepository(UsersRepository);
   }
 
-  async create({ email }: IUsersCreate) {
+  async create(email: string) {
     const userExists = await this.usersRepository.findOne({
       email,
     });
@@ -27,6 +23,14 @@ class UsersService {
     });
 
     await this.usersRepository.save(user);
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne({
+      email,
+    });
 
     return user;
   }
