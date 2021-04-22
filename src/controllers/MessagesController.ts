50720e18484072b310b96df65d17aa8d7ca8aad1
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-04-22 14:59:15 -0300

    get messagens by user id

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -15,6 +15,16 @@ class MessagesController {
 
     return response.json(message);
   }
+
+  async showByUser(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const messagesService = new MessagesService();
+
+    const list = await messagesService.listByUser(id);
+
+    return response.json(list);
+  }
 }
 
 export { MessagesController };
