312e7e045552d40d0bab376c8ed69e567e29ee00
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-04-22 16:16:33 -0300

    refactoring server criation

---
+++ src/http.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
+
+import "./database";
+import { routes } from "./routes";
+
+const app = express();
+
+const publicPath = path.join(__dirname, "..", "public");
+app.use(express.static(publicPath));
+app.set("views", publicPath);
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou ", socket.id);
+});
+
+app.use(express.json());
+
+app.use(routes);
+
+export { http, io };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
-import express from "express";
-
-import "./database";
-import { routes } from "./routes";
+import { http } from "./http";
+import "./websocket/client";
 
 const PORT = 3333;
-const app = express();
 
-app.use(express.json());
-
-app.use(routes);
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+http.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
