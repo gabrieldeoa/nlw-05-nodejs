202c0bd4ebab58a55790a9babe9bcd17e7098050
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-04-22 17:15:36 -0300

    add entity Connection

---
+++ src/entities/Connection.ts
@@ -0,0 +1,46 @@
+import {
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+
+import { v4 as uuid } from "uuid";
+
+import { User } from "./User";
+
+@Entity("connections")
+class Connection {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  socket_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Connection };
