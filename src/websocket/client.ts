c9b4423a4f79f505295402db6bd6d656174fa2d4
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-04-22 17:28:12 -0300

    saving messages on connection  ws

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,14 +1,24 @@
 import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
+import { MessagesService } from "../services/MessagesService";
 import { UsersService } from "../services/UsersService";
+
+interface IParams {
+  text: string;
+  email: string;
+}
 
 io.on("connect", (socket) => {
   const connectionsServices = new ConnectionsService();
   const usersServices = new UsersService();
+  const messagesServices = new MessagesService();
 
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id;
-    const { text, email } = params;
+
+    const { text, email } = params as IParams;
+
+    let user_id = null;
 
     const userExists = await usersServices.findByEmail(email);
 
@@ -16,7 +26,11 @@ io.on("connect", (socket) => {
       const user = await usersServices.create(email);
 
       await connectionsServices.create({ socket_id, user_id: user.id });
+
+      user_id = user.id;
     } else {
+      user_id = userExists.id;
+
       const connection = await connectionsServices.findByUserId(userExists.id);
 
       if (!connection) {
@@ -29,5 +43,7 @@ io.on("connect", (socket) => {
         await connectionsServices.create(connection);
       }
     }
+
+    await messagesServices.create({ text, user_id });
   });
 });
