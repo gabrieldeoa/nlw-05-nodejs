3021d892ceb0c19a30870db1f2c67a241743c86c
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-04-23 15:28:51 -0300

    get all messages

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -9,9 +9,9 @@ interface IParams {
 }
 
 io.on("connect", (socket) => {
-  const connectionsServices = new ConnectionsService();
-  const usersServices = new UsersService();
-  const messagesServices = new MessagesService();
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+  const messagesService = new MessagesService();
 
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id;
@@ -20,30 +20,34 @@ io.on("connect", (socket) => {
 
     let user_id = null;
 
-    const userExists = await usersServices.findByEmail(email);
+    const userExists = await usersService.findByEmail(email);
 
     if (!userExists) {
-      const user = await usersServices.create(email);
+      const user = await usersService.create(email);
 
-      await connectionsServices.create({ socket_id, user_id: user.id });
+      await connectionsService.create({ socket_id, user_id: user.id });
 
       user_id = user.id;
     } else {
       user_id = userExists.id;
 
-      const connection = await connectionsServices.findByUserId(userExists.id);
+      const connection = await connectionsService.findByUserId(userExists.id);
 
       if (!connection) {
-        await connectionsServices.create({
+        await connectionsService.create({
           socket_id,
           user_id: userExists.id,
         });
       } else {
         connection.socket_id = socket_id;
-        await connectionsServices.create(connection);
+        await connectionsService.create(connection);
       }
     }
 
-    await messagesServices.create({ text, user_id });
+    await messagesService.create({ text, user_id });
+
+    const allMessages = await messagesService.listByUser(user_id);
+
+    socket.emit("client_list_all_messages", allMessages);
   });
 });
